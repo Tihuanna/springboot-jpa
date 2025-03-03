dfae3e319712a2a1fba41406123c249af8e890a0
Tihuanna <you@example.com>
2025-03-03 19:59:43 -0300

    Service layer, service registration

--- src/main/java/com/kevin/Curso/resources/UserResource.java
+++ src/main/java/com/kevin/Curso/resources/UserResource.java
@@ -1,21 +1,36 @@
 package com.kevin.Curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kevin.Curso.entities.User;
+import com.kevin.Curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-@GetMapping
-public ResponseEntity<User> findAll(){
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 	
-	User u = new User(1L, "Kevin","kevin@email.com","999999999","12345");
-	return ResponseEntity.ok().body(u);
-}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
 
 }
---
+++ src/main/java/com/kevin/Curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.kevin.Curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kevin.Curso.entities.User;
+import com.kevin.Curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
