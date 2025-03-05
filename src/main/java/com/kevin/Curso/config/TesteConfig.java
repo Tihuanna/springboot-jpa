7fe5a10db0cede2b7b810d7e7b4556c707706785
Tihuanna <you@example.com>
2025-03-05 02:06:38 -0300

    Entidade OrderItem, add ManyToMany pt 2

--- src/main/java/com/kevin/Curso/config/TesteConfig.java
+++ src/main/java/com/kevin/Curso/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.kevin.Curso.entities.Category;
 import com.kevin.Curso.entities.Order;
+import com.kevin.Curso.entities.OrderItem;
 import com.kevin.Curso.entities.Product;
 import com.kevin.Curso.entities.User;
 import com.kevin.Curso.entities.enums.OrderStatus;
 import com.kevin.Curso.repositories.CategoryRepository;
+import com.kevin.Curso.repositories.OrderItemRepository;
 import com.kevin.Curso.repositories.OrderRepository;
 import com.kevin.Curso.repositories.ProductRepository;
 import com.kevin.Curso.repositories.UserRepository;
@@ -32,6 +34,9 @@ public class TesteConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -44,7 +49,7 @@ public class TesteConfig implements CommandLineRunner {
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-		
+
 		User u1 = new User(null, "Maria", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex", "alex@gmail.com", "977777777", "123456");
 
@@ -55,7 +60,7 @@ public class TesteConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
@@ -63,7 +68,14 @@ public class TesteConfig implements CommandLineRunner {
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	}
 }
--- src/main/java/com/kevin/Curso/entities/pk/OrderItemPK.java
+++ src/main/java/com/kevin/Curso/entities/pk/OrderItemPK.java
@@ -15,8 +15,7 @@ import jakarta.persistence.ManyToOne;
 public class OrderItemPK implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@ManyToOne
 	@JoinColumn(name = "order_id")
 	private Order order;
