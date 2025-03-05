6397833d1661e8eea87d2b5526ba0b207ec1f0bb
Tihuanna <you@example.com>
2025-03-05 10:43:17 -0300

    ManyToMany product > orderItem

--- src/main/java/com/kevin/Curso/repositories/OrderItemRepository.java
+++ src/main/java/com/kevin/Curso/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.kevin.Curso.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kevin.Curso.entities.OrderItem;
+import com.kevin.Curso.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
