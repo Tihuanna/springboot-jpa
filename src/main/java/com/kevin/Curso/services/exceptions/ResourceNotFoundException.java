5bc65c78a7ee5b921e2f9077c5a01455a4adc8b9
Tihuanna <you@example.com>
2025-03-05 13:15:35 -0300

    Exception findById 404

---
+++ src/main/java/com/kevin/Curso/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.kevin.Curso.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	private static final long SerialVersionUID = 1L;
+	public ResourceNotFoundException (Object id) {
+		super("Resource not found. id " + id);
+	}
+
+}
