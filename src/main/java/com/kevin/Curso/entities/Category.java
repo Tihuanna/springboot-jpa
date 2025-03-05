1f51a3df3f9b389d6067aae4cb68cc5a2118972f
Tihuanna <you@example.com>
2025-03-05 01:41:00 -0300

    Entidade OrderItem, add ManyToMany

--- src/main/java/com/kevin/Curso/entities/Category.java
+++ src/main/java/com/kevin/Curso/entities/Category.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,7 +26,7 @@ public class Category implements Serializable {
 	private String name;
 	
 	private static final long serialVersionUID = 1L;
-	
+	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
