57656e50458cad739fb5dd22b9ec80aaad3e963a
Tihuanna <you@example.com>
2025-03-05 13:42:41 -0300

    Exception handling delete e update

--- src/main/java/com/kevin/Curso/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/kevin/Curso/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.kevin.Curso.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-	
+
 	private static final long SerialVersionUID = 1L;
 	public ResourceNotFoundException (Object id) {
 		super("Resource not found. id " + id);
