f895ce4f1a8586bbbaa869b65f47289ac4f46c0b
Tihuanna <you@example.com>
2025-03-05 10:53:54 -0300

    Payment, one-to-one

--- src/main/java/com/kevin/Curso/config/TesteConfig.java
+++ src/main/java/com/kevin/Curso/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.kevin.Curso.entities.Category;
 import com.kevin.Curso.entities.Order;
 import com.kevin.Curso.entities.OrderItem;
+import com.kevin.Curso.entities.Payment;
 import com.kevin.Curso.entities.Product;
 import com.kevin.Curso.entities.User;
 import com.kevin.Curso.entities.enums.OrderStatus;
@@ -77,5 +78,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2025-03-03T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
