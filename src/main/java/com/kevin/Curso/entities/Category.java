7f7e9d6da3a1305c3079c14d24478652c08e8d96
Tihuanna <you@example.com>
2025-03-04 21:47:09 -0300

    Join Tables

--- src/main/java/com/kevin/Curso/entities/Category.java
+++ src/main/java/com/kevin/Curso/entities/Category.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 
@@ -24,6 +25,7 @@ public class Category implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category () {
